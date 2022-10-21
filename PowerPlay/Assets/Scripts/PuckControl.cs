860aff57b1d77a8a115b306148eaf81b7c21d4ce
KJWybo018 <kylewybo@gmail.com>
2022-10-20 20:58:14 -0400

    PuckControl

---
+++ PowerPlay/Assets/Scripts/PuckControl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckControl : MonoBehaviour
+{
+    private Rigidbody rbd;
+
+    void GoRandDir()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rbd.AddForce(new Vector3(20,0, 15));
+        }
+        else
+        {
+            rbd.AddForce(new Vector3(-20, 0, -15));
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd = GetComponent<RigidBody>();
+        Invoke("GoRandDir", 2);
+    }
+
+    void ResetPuck()
+    {
+        rbd.velocity = Vector3.zero;
+        transform.postion = Vector3.zero;
+    }
+
+    void RestartGame()
+    {
+        ResetPuck();
+        Invoke("GoRandDir", 1);
+    }
+
+    void OnCollision(Collision coll)
+    {
+        if (coll.collider.CompareTag("Player1") || coll.collider.CompareTag("Player2"))
+        {
+            Vector3 vel;
+            vel.x = rbd.velocity.x;
+            vel.z = (rbd.velocity.z / 2) + (coll.collider.attachedRigidbody.velocit.z / 3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
