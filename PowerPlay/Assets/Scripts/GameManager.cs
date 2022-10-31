3585964cfe22452ad21123813e13e9280a65deb6
KJWybo018 <kylewybo@gmail.com>
2022-10-31 12:33:23 -0400

    New GameManager Script

---
+++ PowerPlay/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+    public GUISkin layout;
+
+    GameObject thePuck;
+    void Start()
+    {
+        thePuck = gameObject.FindGameObjectWithTag("Puck");
+    }
+
+    public static void Score ( string wallID)
+    {
+        if(wallID == "Player2Goal")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Camera.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Camera.width / 2 + 150 = 12, 20, 100, 100), "" + PlayerScore2);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
