615e4db1c42fd725e5d6d87180c145cbd3d09ab1
Ethan Visscher <ecvissch@mtu.edu>
2022-11-03 20:01:10 -0400

    FYSIKS

--- PowerPlay/Assets/Scripts/Player_Movement.cs
+++ PowerPlay/Assets/Scripts/Player_Movement.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float speed = 10.0f;
-    
     private Rigidbody rbd;
-    private Vector3 _startPosition;
-  
+
+    public float force = 100f;
+    public float maxSpeed = 100f;
+
+    public KeyCode moveup = KeyCode.W;
+    public KeyCode movedown = KeyCode.S;
+    public KeyCode moveleft = KeyCode.A;
+    public KeyCode moveright = KeyCode.D;
+    
     void Start()
     {
         rbd = GetComponent<Rigidbody>();
     }
 
-    public void Initialize()
+    void FixedUpdate()
     {
-        _startPosition = transform.position;
-    }
+        rbd.velocity = Vector3.ClampMagnitude(rbd.velocity, maxSpeed);
 
-    void Update()
-    {
-        /*
-        Checks the Player tag and uses the corresponding player movement inputs. 
-        */
-        if(this.CompareTag("Player1")){
-            Vector3 movement_vector = new Vector3(Input.GetAxis("P1_Horizontal"), 0,  Input.GetAxis("P1_Vertical"));
-            if (movement_vector != Vector3.zero){
-            rbd.MovePosition(rbd.position + movement_vector * Time.deltaTime * speed);
-            }
+        if (Input.GetKey(moveup))
+        {
+            rbd.AddForce(0, 0, force);
         }
-
-        if(this.CompareTag("Player2")){
-            Vector3 movement_vector = new Vector3(Input.GetAxis("P2_Horizontal"), 0,  Input.GetAxis("P2_Vertical"));
-            if (movement_vector != Vector3.zero){
-            rbd.MovePosition(rbd.position + movement_vector * Time.deltaTime * speed);
-            }
+        if (Input.GetKey(movedown))
+        {
+            rbd.AddForce(0, 0, -force);
+        }
+        if (Input.GetKey(moveright))
+        {
+            rbd.AddForce(force, 0, 0);
+        }
+        if (Input.GetKey(moveleft))
+        {
+            rbd.AddForce(-force, 0, 0);
         }
     }
 }
